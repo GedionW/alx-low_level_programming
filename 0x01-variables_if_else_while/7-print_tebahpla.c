2945b614e9acaaeea85795cb012af9f41c860f77
GedionW <yo>
2022-09-09 05:50:45 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stidio.h>
+
+/**
+ *
+ *  * main - Prints the alphabet at reverse
+ *
+ *     * Return: Always (Success)
+ *
+ *     */
+int main(void)
+
+{
+	char c;
+
+	for (c = 'z'' c >= 'a'; c--);
+	   
+	   {
+		   putchar(c);
+	   }
+	    putchar('\n');
+
+	    return (0);
+}
