c4af1ae4fe6dfddfa7bcf2a4ca7de96097e69e44
GedionW <wambuagideon19@gmail.com>
2022-09-13 06:57:01 -0700

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define HOLBERTON_H_INCLUDED
 
 int _putchar(char);
-void print_laphabet(void);
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _isalpha_sign(int n);
 int _abs(int);
