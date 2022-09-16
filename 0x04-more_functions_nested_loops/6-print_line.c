d0378979d48ab07ef627d21b5b9aabd22966d98d
GedionW <wambuagideon19@gmail.com>
2022-09-16 08:34:58 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,49 +1,17 @@
+#include <stdio.h>
 #include "main.h"
 
-
-
 /**
- *
- *  * print_line - prints a line
- *
- *   * @n: integer
- *
- *    * Return: always void
- *
- *     */
-
-
+*print_line - prints a straight line
+*@n: parameter
+*Return:returns nothing
+*/
 
 void print_line(int n)
-
-	  
-
 {
-
-	  
-
-	  int i;
-
-	    
-
-	    for (i = 0; i < n; i++)
-
-		        
-
-		        {
-
-				      
-
-				      _putchar('_');
-
-				            
-
-				          }
-
-	      
-
-	      _putchar('\n');
-
-	        
-
+	while (n-- > 0)
+	{
+		putchar('_');
+	}
+	putchar('\n');
 }
