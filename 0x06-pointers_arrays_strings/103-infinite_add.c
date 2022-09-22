d8322864d825c1cd82b787ee1620221c802c007c
GedionW <wambuagideon19@gmail.com>
2022-09-22 13:58:25 -0700

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -36,5 +36,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	}
 	r[k] = '\0';
 	if (i >= 0 || j >= 0 || m)
-		return (r);
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
+	return (r);
 }
