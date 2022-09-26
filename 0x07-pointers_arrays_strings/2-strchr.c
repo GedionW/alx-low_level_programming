65d23c55e5f05775279a8dcc8560fd1d7cfdbcaf
GedionW <wambuagideon19@gmail.com>
2022-09-26 05:15:31 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to check
+ * @c: character to check for
+ *
+ * Return: pointer to check s with c or null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = o; *(s + i); i++)
+	{
+		if (*(s(s + i) == c)
+			return (s + i);
+	}
+	if (*(s + i) == c)
+		return (s + i);
+	return (0);
+}
