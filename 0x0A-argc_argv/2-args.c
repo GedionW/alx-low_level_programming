d3684c1b1db78f9839bd33f4bc0e035608b45b14
GedionW <wambuagideon19@gmail.com>
2022-09-30 03:05:57 -0700

    printing arguments passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints arguments received
+ * @argc: argc parameter
+ * @argv: array of command listed
+ * Return: 0 
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
