4d721e9a710bcaaee3ee986b577e7a9a964c51ad
GedionW <wambuagideon19@gmail.com>
2022-10-04 10:38:02 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,32 +1,41 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
  * _strdup - return pointer tonewly allocate space
- * which contains a copy of the string given as a parameter
- * @str:string to be copied
+ *which contains a copy of the string given as a parameter
+ *@str:string to be copied
  *
- *Return:NULL if str 
+ *Return:NULL if str
+ *Null if memory is unsufficient
  */
 char *_strdup(char *str)
 {
-	char *cpy;
-	int index, len;
+	char *nstr;
+	unsigned int len, i;
 
 	if (str == NULL)
-		return (NULL);
-
-	for (index = 0; str[index]; index++)
-		len++;
-	cpy = malloc(sizeof(char) * (len + 1));
-	if (cpy == NULL)
-		return (NULL);
-
-	for (index = 0; str[index]; index++)
 	{
-		cpy[index] = str[index];
+		return (NULL);
 	}
 
-	cpy[len] = '\0';
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
 
-	return (cpy);
+	nstr = malloc(sizeof(char) * (len + 1));
+
+	/*check if malloc was successful*/
+	if (nstr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		nstr[i] = str[i];
+	}
+	nstr[len] = '\0';
+	return (nstr);
 }
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -9,7 +9,7 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 int **alloc_grind(int width, int height);
 void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
+char **argstostr(int ac, char **av);
 char **strtow(char *str);
 
 #endif /* MAIN_H */
