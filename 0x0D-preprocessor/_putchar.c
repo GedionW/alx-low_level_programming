c43277b4ffead72e08256c4d5d23051a61bc3f5c
GedionW <wambuagideon19@gmail.com>
2022-10-12 12:34:04 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/_putchar.c
@@ -0,0 +1,13 @@
+#include <unstd.h>
+
+/**_putchar - writes the character stdout
+ * @c: charcter to print
+ * Return: 1. on success
+ * On error, -1 is returned and errno is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
+ 
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_
+
+#include <stdio.h>
+#include "0-object_like_macro.h"
+#include "pi.h"
+#include "3-function_like_macro.h"
+#include "4-sum.h"
+
+#endif
